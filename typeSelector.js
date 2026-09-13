f23facfa8411887ca343c7ab205be4cd83934806
mepul <sepulvedamorgan2@gmail.com>
2026-09-13 18:24:33 -0500

    2.0 Functionality Complete

---
+++ typeSelector.js
@@ -0,0 +1,46 @@
+const mobileTypeSelector = document.getElementById('mobileTypeSelector');
+const typeCardHolder = document.querySelector('.typecardholder');
+const typeCards = document.querySelectorAll('.typecard')
+
+mobileTypeSelector.value = 'normal';
+
+mobileTypeSelector.addEventListener('change', (e) => {
+    mobileTypeSelectorUpdater()
+})
+
+function mobileTypeSelectorUpdater() {
+    const selectedType = mobileTypeSelector.value
+
+    for (const typeCard of typeCards) {
+
+        typeCard.classList.add('d-none')
+
+        if (typeCard.dataset.poketype === selectedType) {
+            typeCard.classList.add('d-flex')
+            typeCard.classList.remove('d-none')
+        }
+    }
+
+}
+
+mobileTypeSelectorUpdater()
+
+
+window.addEventListener('resize', (e) => {
+    windowWidth()
+})
+
+function windowWidth() {
+    if (window.innerWidth >= 576) {
+        for (const typeCard of typeCards) {
+
+            typeCard.classList.add('d-flex')
+            typeCard.classList.remove('d-none')
+
+        }
+    } else{
+        mobileTypeSelectorUpdater()
+
+    }
+}
+windowWidth()
